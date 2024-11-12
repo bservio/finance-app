7b10321e572885c0099b893c178f84bf1892cacd
Bruno Sérvio <brunoservio21@gmail.com>
2024-11-12 10:58:00 -0300

    feat: add pie chart to dashboard

--- app/(home)/_components/summary-cards.tsx
+++ app/(home)/_components/summary-cards.tsx
@@ -5,62 +5,23 @@ import {
   WalletIcon,
 } from "lucide-react";
 import SummaryCard from "./summary-card";
-import { db } from "@/app/_lib/prisma";
 
 interface SummaryCardProps {
   month: string;
+  balance: number;
+  depositsTotal: number;
+  investmentsTotal: number;
+  expensesTotal: number;
 }
 
-const SummaryCards = async ({ month }: SummaryCardProps) => {
-  const where = {
-    date: {
-      gte: new Date(`2024-${month}-01`),
-      lt: new Date(`2024-${month}-31`),
-    },
-  };
-  const depositsTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: {
-          ...where,
-          type: "DEPOSIT",
-        },
-        _sum: {
-          amount: true,
-        },
-      })
-    )?._sum?.amount,
-  );
-  const investmentsTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: {
-          ...where,
-          type: "INVESTMENT",
-        },
-        _sum: {
-          amount: true,
-        },
-      })
-    )?._sum?.amount,
-  );
-  const expensesTotal = Number(
-    (
-      await db.transaction.aggregate({
-        where: {
-          ...where,
-          type: "EXPENSE",
-        },
-        _sum: {
-          amount: true,
-        },
-      })
-    )?._sum?.amount,
-  );
-  const balance = depositsTotal - investmentsTotal - expensesTotal;
-
+const SummaryCards = async ({
+  balance,
+  depositsTotal,
+  investmentsTotal,
+  expensesTotal,
+}: SummaryCardProps) => {
   return (
-    <>
+    <div className="space-y-6">
       <SummaryCard
         icon={<WalletIcon size={16} />}
         title="Saldo"
@@ -87,7 +48,7 @@ const SummaryCards = async ({ month }: SummaryCardProps) => {
           amount={expensesTotal}
         />
       </div>
-    </>
+    </div>
   );
 };
 
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,8 @@ import NavBar from "../_components/nav-bar";
 import MontSelect from "./_components/month-select";
 import SummaryCards from "./_components/summary-cards";
 import { isMatch } from "date-fns";
+import TransactionsPieChart from "./_components/transactions-pie-chart";
+import { getDashboard } from "../_data/get-dashboard";
 
 interface HomeProps {
   searchParams: {
@@ -21,6 +23,8 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
     redirect("?month=01");
   }
 
+  const dashboard = await getDashboard(month);
+
   return (
     <>
       <NavBar />
@@ -29,7 +33,15 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <MontSelect />
         </div>
-        <SummaryCards month={month} />
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="space-y-6">
+            <SummaryCards month={month} {...dashboard} />
+
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionsPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
